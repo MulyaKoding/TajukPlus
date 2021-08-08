5ffdc30c59951c1f786b093e7e917e0d21c3c81f
RahmatMulyaSimanjuntak <rahmatsnsd@gmail.com>
2021-08-08 21:22:51 +0700

    followingGroup

--- src/app/following-group/following-group.page.ts
+++ src/app/following-group/following-group.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Button } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-following-group',
   templateUrl: './following-group.page.html',
   styleUrls: ['./following-group.page.scss'],
 })
-export class FollowingGroupPage implements OnInit {
+export class FollowingGroupPage  {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
-  ngOnInit() {
+  
+  async btnClick(){
+    const alert = await this.alertController.create({
+      header: 'Anda yakin ingin keluar group?',
+      buttons: ['Ya','Tidak']
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+    console.log(result);
   }
 
 }
