4c4275ecdca570649149b4b16e64bf8e72940813
rahmatsnsd <rahmatsnsd@gmail.com>
2021-08-16 13:54:05 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./help/help.module').then( m => m.HelpPageModule)
+    loadChildren: () => import('./pengaturan/pengaturan.module').then( m => m.PengaturanPageModule)
   },
   {
     path: '',
-    redirectTo: 'help',
+    redirectTo: 'pengaturan',
     pathMatch: 'full'
   },
   {
